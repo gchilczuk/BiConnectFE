b5403d005757ec6e7275a5e366307db7cb0aa134
marwaw <martyna.waw96@gmail.com>
2018-04-06 17:33:25 +0200

    Meeting picker and main page for a secretary

---
+++ pages/secretaryMain.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>Spotkania</h1>
+      <meeting-picker/>
+      <el-button type="success"  v-on:click="addNew()">Dodaj wystąpienie</el-button>
+    </div>
+</template>
+
+<script>
+    import MeetingPicker from "../components/MeetingPicker";
+
+    export default {
+      components: {MeetingPicker},
+      name: "secretary-main",
+      methods: {
+        addNew() {
+          alert('Spotkanie dodane');
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
