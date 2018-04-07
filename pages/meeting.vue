b8c8511df9ef004924b26e104f680624cde06bf5
Paweł P <panczo12d@gmail.com>
2018-04-07 17:01:16 +0200

    speeches list

--- pages/meeting.vue
+++ pages/meeting.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    meet
+    <el-container>
+      <div>
+        <el-aside width="600px">
+          <speeches-list></speeches-list>
+        </el-aside>
+      </div>
+      <el-main>
+        main
+      </el-main>
+    </el-container>
+
   </div>
 </template>
 
 <script>
+  import SpeechesList from "../components/SpeechesList";
 
+  export default {
+    components: {
+      SpeechesList
+    }
+
+  }
 </script>
