ef4fde1ab0b1129643234258e20786336e12f595
Paweł P <panczo12d@gmail.com>
2018-04-22 01:02:26 +0200

    code refactor

--- pages/meetings-menu.vue
+++ pages/meetings-menu.vue
@@ -1,9 +1,8 @@
 <template>
   <b-container class="mt-4">
     <h1>Spotkania</h1>
-    <el-date-picker type="date" v-model="meetingDate" format="dd.MM.yyyy" value-format="yyyy-MM-dd"></el-date-picker>
+    <el-date-picker type="date" v-model="meetingDate" format="dd.MM.yyyy" value-format="yyyy-MM-dd"/>
     <el-button class="ml-2" @click="addNewMeeting" type="success" plain>Dodaj spotkanie</el-button>
-
     <el-table
       :data="tableData"
       highlight-current-row
@@ -39,7 +38,7 @@
     name: "meetings-menu",
     data() {
       return {
-          meetingDate: this.getToday()
+        meetingDate: this.getToday()
       }
     },
     mounted() {
@@ -52,19 +51,17 @@
     },
     methods: {
       getToday: function () {
-        var today = new Date()
-        var dd = today.getDate()
-        var mm = today.getMonth() + 1
-        var yyyy = today.getFullYear()
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1;
+        const yyyy = today.getFullYear();
 
         if (dd < 10)
           dd = '0' + dd
         if (mm < 10)
           mm = '0' + mm
 
-        var today = dd + '-' + mm + '-' + yyyy
-        console.log("set cd" + today)
-        return today
+        return yyyy + '-' + mm + '-' + dd
       },
       async handleRowClicked(row, event, column) {
         if (event.target.nodeName !== 'BUTTON') {
@@ -74,14 +71,11 @@
         }
       },
       handleDelete(index, row) {
-        this.$store.dispatch('meetings/removeMeeting', index)
+        this.$store.dispatch('meetings/removeMeeting', row.id)
       },
-      addNewMeeting(){
+      addMeeting() {
         this.$store.dispatch('meetings/addMeeting', this.meetingDate)
       }
-    },
-    mounted() {
-      this.$store.dispatch('meetings/fetchMeetings')
     }
   }
 </script>
