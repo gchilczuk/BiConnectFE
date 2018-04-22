055569b3e219c368450fcfe70af4b31c4c27f720
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-04-23 00:38:06 +0200

    alerts

--- components/SpeechForm.vue
+++ components/SpeechForm.vue
@@ -100,8 +100,8 @@
             text: "W formularzu istnieją niezapisane dane!",
             type: 'warning',
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
             confirmButtonText: 'Odrzuć zmiany!',
             cancelButtonText: 'Powróć do formularza',
             reverseButtons: true
