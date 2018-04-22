3394552df560beef34bc0af67cadc06a16ae3f53
pawel <panczo12d@gmail.com>
2018-04-23 00:53:14 +0200

    fix get person info

--- components/SpeechForm.vue
+++ components/SpeechForm.vue
@@ -19,7 +19,7 @@
       <el-form-item label="Osoba ">
           <el-autocomplete
             class="inline-input"
-            v-model="speech.people"
+            v-model="personInput"
             :fetch-suggestions="querySearch"
             value-key="search_key"
             placeholder="Please Input"
@@ -86,6 +86,7 @@
     data() {
       return {
         people: null,
+        personInput: null,
         speech: {
           person: null,
           requirements: [],
@@ -97,25 +98,26 @@
     },
     async mounted() {
       const result = await this.$axios.get('http://biconnect.herokuapp.com/people')
-      result.data.forEach(function(e) { e.search_key = e.first_name + ' ' + e.last_name; });
+      result.data.forEach(function(e) { e.search_key = e.first_name + ' ' + e.last_name; })
       this.people = result.data
     },
     watch: {
       activeSpeechTableInd: function () {
         this.speech = this.$store.getters['meetings/activeSpeech']
+        this.personInput = this.speech.person.first_name + ' ' + this.speech.person.last_name,
         this.requirementsCounter = this.speech && this.speech.requirements && this.speech.requirements.length
         this.recommendationsCounter = this.speech && this.speech.recommendations && this.speech.recommendations.length
       }
     },
     methods: {
       querySearch(queryString, cb) {
-        const results = queryString ? this.people.filter(this.createFilter(queryString)) : this.people;
+        const results = queryString ? this.people.filter(this.createFilter(queryString)) : this.people
         cb(results);
       },
       createFilter(queryString) {
         return (people) => {
           const name = people.first_name + ' ' + people.last_name
-          return (name.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+          return (name.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
         };
       },
       handleSelect(item) {
